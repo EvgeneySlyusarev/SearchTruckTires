6a96cbbcf7e52ba277ff9b7684bbb54bef8db0e0
EvgeneySlyusarev <80619015+EvgeneySlyusarev@users.noreply.github.com>
2021-11-21 17:39:28 +0200

    Add project files.

---
+++ SearchTruckTires/SearchTruckTires/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SearchTruckTires
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            count--;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
