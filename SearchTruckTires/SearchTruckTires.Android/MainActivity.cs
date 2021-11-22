89eae287da1838695ec84cab48d522e4d31fb69d
Evgenei Slyusarev <slyusarev.e@gmail.com>
2021-11-22 16:38:45 +0200

    FindTires

--- SearchTruckTires/SearchTruckTires.Android/MainActivity.cs
+++ SearchTruckTires/SearchTruckTires.Android/MainActivity.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
@@ -7,7 +6,7 @@ using Android.OS;
 
 namespace SearchTruckTires.Droid
 {
-    [Activity(Label = "SearchTruckTires", Icon = "@mipmap/logo_parsing", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "FindTires", Icon = "@mipmap/logo_parsing", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
