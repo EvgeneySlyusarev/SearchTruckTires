a7749e4b2735f989e1dfcc33ea41cc8f217a98a9
Evgenei Slyusarev <slyusarev.e@gmail.com>
2021-11-22 16:31:23 +0200

    LOGO

--- SearchTruckTires/SearchTruckTires.Android/MainActivity.cs
+++ SearchTruckTires/SearchTruckTires.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace SearchTruckTires.Droid
 {
-    [Activity(Label = "SearchTruckTires", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "SearchTruckTires", Icon = "@mipmap/logo_parsing", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
