4518c4a4ab7e8b452cf900625e5f478eb33b216b
Evgenei Slyusarev <slyusarev.e@gmail.com>
2021-11-23 13:11:57 +0200

    Arrey_size

--- SearchTruckTires/SearchTruckTires.Android/MainActivity.cs
+++ SearchTruckTires/SearchTruckTires.Android/MainActivity.cs
@@ -3,25 +3,40 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Widget;
 
 namespace SearchTruckTires.Droid
 {
     [Activity(Label = "FindTires", Icon = "@mipmap/logo_parsing", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate(bundle);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            // Set our view from the "Main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.spinner);
+
+            spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            var adapter = ArrayAdapter.CreateFromResource(
+                    this, Resource.Array.TireSizes_array, Android.Resource.Layout.SimpleSpinnerItem);
+
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = adapter;
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = (Spinner)sender;
+            string toast = string.Format("Selected tire size {0}", spinner.GetItemAtPosition(e.Position));
+            Toast.MakeText(this, toast, ToastLength.Long).Show();
         }
     }
 }
--- SearchTruckTires/SearchTruckTires/MainPage.xaml.cs
+++ SearchTruckTires/SearchTruckTires/MainPage.xaml.cs
@@ -1,37 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace SearchTruckTires
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        public int count = 0;
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            count++;
-            ((Button)sender).Text = $"You clicked + {count} times.";
-        }
-        private void Button_Clicked_1(object sender, EventArgs e)
-        {
-            count--;
-            ((Button)sender).Text = $"You clicked - {count} times.";
-        }
-        private void Button_Clicked_2(object sender, EventArgs e)
-        {
-            count *= count;
-            ((Button)sender).Text = $"Count - {count} times.";
-            ((Button)sender).Text = $"You clicked - {count} times.";
         }
     }
 }
