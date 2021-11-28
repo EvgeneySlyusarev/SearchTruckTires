d0072c9e4805c947c3e916509d82860ae61c8ed1
Evgenei Slyusarev <slyusarev.e@gmail.com>
2021-11-28 21:31:49 +0200

    pathParsing is working

--- SearchTruckTires/SearchTruckTires.Android/MainActivity.cs
+++ SearchTruckTires/SearchTruckTires.Android/MainActivity.cs
@@ -14,27 +14,27 @@ namespace SearchTruckTires.Droid
     [Activity(Label = "FindTires", Icon = "@mipmap/logo_parsing", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        ListView listView;
-        List<string> list;
-
+        private ListView listView;
+        private List<string> list;
+        private List<string> produkt;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             // Set our view from the "Main" layout resource 
             SetContentView(Resource.Layout.Main);
 
+            //set spiner
             Spinner spinner = FindViewById<Spinner>(Resource.Id.spinner);
-
             spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
             var adapter = ArrayAdapter.CreateFromResource(this, Resource.Array.TireSizes_array, Android.Resource.Layout.SimpleSpinnerItem);
-
             adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
             spinner.Adapter = adapter;
-
+             
+            // set listView
             list = new List<string>();
-            list.AddRange(new string[] { "iPhone 7", "Samsung Galaxy S8", "Huawei P10", "LG G6" });
+            produkt = new List<string>();
+            list.AddRange(produkt);
             listView = FindViewById<ListView>(Resource.Id.listView);
-
             ArrayAdapter<string> adapter1 = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, list);
             listView.Adapter = adapter1;
 
@@ -48,37 +48,27 @@ namespace SearchTruckTires.Droid
         private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
         {
             Spinner spinner = (Spinner)sender;
-            string toast = string.Format("Selected tire size {0}", spinner.GetItemAtPosition(e.Position));
+            //string toast = string.Format("Selected tire size {0}", spinner.GetItemAtPosition(e.Position));
+            string toast = string.Format((string)spinner.GetItemAtPosition(e.Position));
             Toast.MakeText(this, toast, ToastLength.Long).Show();
-            //PathParsing(toast);
+            PathParsing(toast);
         }
         private void PathParsing(string toast)
         {
-            var url = "http://mpk-tyres.com.ua/catalog/38565R225/";
+            string standardSize;
+            standardSize = toast.Replace("/", "");
+            standardSize = standardSize.Replace(".", "");
+            var url = "http://mpk-tyres.com.ua/catalog/" + standardSize + "/";
 
             HtmlWeb web = new HtmlWeb();
             var htmlDoc = web.Load(url);
             var page = htmlDoc.DocumentNode;
-
             var nodes = page.QuerySelectorAll("li.product");
 
-        //    ListView listView = FindViewById<ListView>(Resource.Id.listView1);
-
-        //    var data = new List<string>();
-
-        //    ArrayAdapter<String> adapter = new ArrayAdapter<string>(this,
-        //Resource.Id.listView1, data);
-
-        //    data.Add("Product 1"); 
-        //    data.Add("Product 2");
-            
-            //foreach (var item in nodes) // поиск в файле данных
-            //{
-            //    var title = item.QuerySelector("h3 a").InnerText.Trim();
-            //    var prise = item.QuerySelector("span.price").InnerText.Trim();
-            //    data.Add(title);
-            //}
-
+            foreach (var item in nodes)
+            {
+                produkt.Add(item.InnerText);
+            }
         }
     }
 }
