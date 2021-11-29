101a128d6105c95e61855a31ad63609e9b88f46f
Evgenei Slyusarev <slyusarev.e@gmail.com>
2021-11-29 16:08:14 +0200

    is`t working

--- SearchTruckTires/SearchTruckTires.Android/MainActivity.cs
+++ SearchTruckTires/SearchTruckTires.Android/MainActivity.cs
@@ -14,11 +14,12 @@ namespace SearchTruckTires.Droid
     [Activity(Label = "FindTires", Icon = "@mipmap/logo_parsing", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        private ListView listView;
-        private List<string> list;
-        private List<string> produkt;
+        public ListView listView;
+        public List<string> produkt;
+
         protected override void OnCreate(Bundle bundle)
         {
+            
             base.OnCreate(bundle);
             // Set our view from the "Main" layout resource 
             SetContentView(Resource.Layout.Main);
@@ -26,16 +27,17 @@ namespace SearchTruckTires.Droid
             //set spiner
             Spinner spinner = FindViewById<Spinner>(Resource.Id.spinner);
             spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
-            var adapter = ArrayAdapter.CreateFromResource(this, Resource.Array.TireSizes_array, Android.Resource.Layout.SimpleSpinnerItem);
+            ArrayAdapter adapter = ArrayAdapter.CreateFromResource(this, Resource.Array.TireSizes_array, Android.Resource.Layout.SimpleSpinnerItem);
             adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
             spinner.Adapter = adapter;
-             
+
             // set listView
-            list = new List<string>();
-            produkt = new List<string>();
-            list.AddRange(produkt);
+            
             listView = FindViewById<ListView>(Resource.Id.listView);
-            ArrayAdapter<string> adapter1 = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, list);
+            //produkt.AddRange(new string[] { "sdkjbskjd", "dhajehdaje" });
+            produkt = new List<string>();
+            produkt.AddRange(produkt);
+            ArrayAdapter<string> adapter1 = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, produkt);
             listView.Adapter = adapter1;
 
         }
@@ -48,26 +50,27 @@ namespace SearchTruckTires.Droid
         private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
         {
             Spinner spinner = (Spinner)sender;
-            //string toast = string.Format("Selected tire size {0}", spinner.GetItemAtPosition(e.Position));
             string toast = string.Format((string)spinner.GetItemAtPosition(e.Position));
             Toast.MakeText(this, toast, ToastLength.Long).Show();
-            PathParsing(toast);
+            Parsing(toast);
         }
-        private void PathParsing(string toast)
+        private void Parsing(string toast)
         {
             string standardSize;
             standardSize = toast.Replace("/", "");
             standardSize = standardSize.Replace(".", "");
+            standardSize = standardSize.ToLower();
             var url = "http://mpk-tyres.com.ua/catalog/" + standardSize + "/";
-
+        
             HtmlWeb web = new HtmlWeb();
             var htmlDoc = web.Load(url);
             var page = htmlDoc.DocumentNode;
-            var nodes = page.QuerySelectorAll("li.product");
 
-            foreach (var item in nodes)
+            foreach (var item in page.QuerySelectorAll("li.product")) // поиск в файле данных
             {
-                produkt.Add(item.InnerText);
+                string title = item.QuerySelector("div.product_info a").InnerText.Trim();
+                string prise = item.QuerySelector("td.price-td").InnerText.Trim();
+                produkt.Add(title);
             }
         }
     }
