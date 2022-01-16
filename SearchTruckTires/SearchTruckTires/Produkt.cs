e77db192873299edfc956575251b0bc082c5a9d0
Evgenei Slyusarev <slyusarev.e@gmail.com>
2022-01-16 12:56:34 +0200

    Заработал Вывод картинок продукта

--- SearchTruckTires/SearchTruckTires/Produkt.cs
+++ SearchTruckTires/SearchTruckTires/Produkt.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using System.IO;
+using System.Net;
 using Xamarin.Forms;
 
 namespace SearchTruckTires
@@ -9,6 +11,16 @@ namespace SearchTruckTires
         public string PriseN { get; set; }
         public string PriseBN { get; set; }
         public string ImageURL { get; set; }
-        public Image ImageProdukt { get; set; }
+        public ImageSource ImageProdukt
+        {
+            get
+            {
+                WebClient Client = new WebClient();
+
+                var byteArray = Client.DownloadData(ImageURL);
+                return ImageSource.FromStream(() => new MemoryStream(byteArray));
+                //return ImageSource.FromUri(new System.Uri("https://mpk-tyres.com.ua/files/products/stormers216_2021.200x200.jpg"));
+            }
+        }
     }
 }
--- SearchTruckTires/SearchTruckTires/Tires.xaml.cs
+++ SearchTruckTires/SearchTruckTires/Tires.xaml.cs
@@ -20,44 +20,48 @@ namespace SearchTruckTires
             BindingContext = this;
             myListView.HasUnevenRows = true;
             // Определяем формат отображения данных
-            myListView.ItemTemplate = new DataTemplate(() =>
-            {
-                // привязка к свойству Title
-                Label title = new Label { FontSize = 18, FontAttributes = FontAttributes.Bold };
-                title.SetBinding(Label.TextProperty, "Title");
+         
+            //myListView.ItemTemplate = new DataTemplate(() =>
+            //{
+            //    // привязка к свойству Title
+            //    Label title = new Label { FontSize = 18, FontAttributes = FontAttributes.Bold };
+            //    title.SetBinding(Label.TextProperty, "Title");
 
-                // привязка к свойству PriseN
-                Label priseN = new Label { FontAttributes = FontAttributes.Bold};
-                priseN.SetBinding(Label.TextProperty, "PriseN");
+            //    // привязка к свойству PriseN
+            //    Label priseN = new Label { FontAttributes = FontAttributes.Bold};
+            //    priseN.SetBinding(Label.TextProperty, "PriseN");
 
-                // привязка к свойству PriceBN
-                Label priseBN = new Label { FontAttributes = FontAttributes.Bold };
-                priseBN.SetBinding(Label.TextProperty, "PriseBN");
+            //    // привязка к свойству PriceBN
+            //    Label priseBN = new Label { FontAttributes = FontAttributes.Bold };
+            //    priseBN.SetBinding(Label.TextProperty, "PriseBN");
 
-                Image imageURL = new Image();
-                imageURL.SetBinding(BindingContextProperty, "ImageProdukt");
+            //    Label url = new Label { FontAttributes = FontAttributes.Bold };
+            //    url.SetBinding(Label.TextProperty, "ImageURL");
 
-                // создаем объект ViewCell.
-                return new ViewCell
-                {
-                    View = new StackLayout
-                    {
-                        Padding = new Thickness(0, 10),
-                        Orientation = StackOrientation.Vertical,
-                        Children = { title, priseN, priseBN, imageURL }
-                    }
-                };
-            });
+            //    Image imageURL = new Image();
+            //    imageURL.Source = ImageSource.FromUri(new Uri("ImageURL"));
+            //    //imageURL.SetBinding(Image.SourceProperty, "ImageProdukt");
+            //    // создаем объект ViewCell.
+            //    return new ViewCell
+            //    {
+            //        View = new StackLayout
+            //        {
+            //            Padding = new Thickness(0, 10),
+            //            Orientation = StackOrientation.Vertical,
+            //            Children = { title, priseN, priseBN, url, imageURL }
+            //        }
+            //    };
+            //});
         }
-        public Image LoadImage(string ImageURL)
-        {
-            Image image = new Image
-            {
-                Source = ImageSource.FromUri(new Uri(ImageURL))
-            };
-            return image;
-        }
-       
+        //public Image LoadImage(string ImageURL)
+        //{
+        //    Image image = new Image
+        //    {
+        //        Source = ImageSource.FromUri(new Uri(ImageURL))
+        //    };
+        //    return image;
+        //}
+
         public async void OnItemTapped(object sender, ItemTappedEventArgs e)
         {
             //if (e.Item is Produkt selectedProdukt)
@@ -107,7 +111,7 @@ namespace SearchTruckTires
                 priceBN = RoundUP(Convert.ToInt32(priceBN));
                 string priseNUP = " НАЛ - " + Convert.ToString(Convert.ToInt32(priceN)) + " ГРН , ";
                 string priseBNUP = " с НДС - " + Convert.ToString(Convert.ToInt32(priceBN)) + " ГРН.";
-                produkts.Add(new Produkt { Title = title, PriseN = priseNUP, PriseBN = priseBNUP, ImageURL = imageURL , ImageProdukt = LoadImage("https://aka.ms/campus.jpg")});
+                produkts.Add(new Produkt { Title = title, PriseN = priseNUP, PriseBN = priseBNUP, ImageURL = imageURL });
                 //http://mpk-tyres.com.ua/files/products/stormers216_2021.200x200.jpg?be6e55ef64de826f63e78dc19421fe5f и эта ссылка не работает
                 //https://aka.ms/campus.jpg - Эта ссылка работает!!!!!!!!!!!!!!!!!!
                 //https://i.ytimg.com/an_webp/0EsUUeW6V-8/mqdefault_6s.webp?du=3000&sqp=CNiY8o4G&rs=AOn4CLD_tqKpVedvB8-rjbQCO6tF8C2FLQ и даже эта работает!!!!!!!!!!!!!!!!!!!!!!!!!
