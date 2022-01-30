76ea2a165261859e81d7b33537a36a31cc2f406e
Evgenei Slyusarev <slyusarev.e@gmail.com>
2022-01-30 20:37:34 +0200

    Парсинг сайта Капитан начало работы создан метод (пробный МПК)

--- SearchTruckTires/SearchTruckTires/Tires.xaml.cs
+++ SearchTruckTires/SearchTruckTires/Tires.xaml.cs
@@ -32,11 +32,53 @@ namespace SearchTruckTires
         private void PickerTires_SelectedIndexChanged(object sender, EventArgs e)
         {
             ParsingMPKTires(pickerTires.Items[pickerTires.SelectedIndex].ToString());
+            ParsingKapitan(pickerTires.Items[pickerTires.SelectedIndex].ToString());
         }
       
         private void ParsingMPKTires(string toast)
         {
             produktsTires.Clear();
+            string standardSize;
+            standardSize = toast.Replace("/", "");
+            standardSize = standardSize.Replace(".", "");
+            standardSize = standardSize.ToLower();
+            string url = "http://mpk-tyres.com.ua/catalog/" + standardSize + "/";
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument htmlDoc = web.Load(url);
+            HtmlNode page = htmlDoc.DocumentNode;
+
+            int RoundUP(int value)
+            {
+                value = value + 100 - (value % 100);
+                return value;
+            }
+
+            foreach (HtmlNode item in page.QuerySelectorAll("li.product")) // поиск в файле данных
+            {
+                string imageURL = item.QuerySelector("img").GetAttributeValue("src", null);
+                imageURL = imageURL.Substring(0, imageURL.IndexOf('?'));
+                string title = item.QuerySelector("div.product_info a").InnerText.Trim();
+                string strPrice = item.QuerySelector("td.price-td").InnerText.Trim();
+                strPrice = strPrice.Replace(" ", "");
+                strPrice = strPrice.Replace("грн", "");
+                double priceBN = Convert.ToDouble(strPrice);
+                double priceN = Convert.ToDouble(strPrice);
+                double marginBN = 1.1;
+                double marginN = 1.05;
+                priceBN *= marginBN;
+                priceN *= marginN;
+                priceN = RoundUP(Convert.ToInt32(priceN));
+                priceBN = RoundUP(Convert.ToInt32(priceBN));
+                string priseNUP = " НАЛ - " + Convert.ToString(Convert.ToInt32(priceN)) + " ГРН , ";
+                string priseBNUP = " с НДС - " + Convert.ToString(Convert.ToInt32(priceBN)) + " ГРН.";
+                produktsTires.Add(new ProduktTires { Title = title, PriseN = priseNUP, PriseBN = priseBNUP, ImageURL = imageURL });
+            }
+
+        }
+        private void ParsingKapitan(string toast)// Нужно изменить для парсинга капитан
+        {
+            //produktsTires.Clear();
             string standardSize;
             standardSize = toast.Replace("/", "");
             standardSize = standardSize.Replace(".", "");
