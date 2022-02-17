77cd5828fc580b76e99a74dea3a76c5e10f064a3
Evgenei Slyusarev <slyusarev.e@gmail.com>
2022-02-17 17:02:10 +0200

    todo

--- SearchTruckTires/SearchTruckTires/Pages/Basket.xaml.cs
+++ SearchTruckTires/SearchTruckTires/Pages/Basket.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Collections.ObjectModel;
+using System;
+using System.Linq;
 
 namespace SearchTruckTires
 {
@@ -10,6 +12,9 @@ namespace SearchTruckTires
     {
         public static readonly ObservableCollection<ProduktBasket> produktsBasket = new ObservableCollection<ProduktBasket>();
         public static int DeleteBasketClicked { get; set; }
+        public static int CountProduktsBasket { get; private set; }
+        public static int СostProductsCart { get; private set; }
+
         public Basket()
         {
             BackgroundImageSource = "@Resources/Drawable/WheelMark2.png";
@@ -20,9 +25,29 @@ namespace SearchTruckTires
             ListViewBasket.HasUnevenRows = true;
         }
 
+        public int ProductBasketCounter()
+        {
+            for (int i = 0; i < produktsBasket.Count; i++)
+            {
+                CountProduktsBasket += produktsBasket[i].QuantityProduktBasket;
+            }
+            return CountProduktsBasket;
+        }
+
+        public int ProductsCartСost(ObservableCollection<ProduktBasket> produktsBasket)
+        {
+            СostProductsCart = 0;
+            for (int i = 0; i < produktsBasket.Count; i++)
+            {
+                int.TryParse(string.Join("", produktsBasket[i].PriseNProduktBasket.Where(c => char.IsDigit(c))), out int value);
+                СostProductsCart += value;
+            }
+            return СostProductsCart;
+        }
+
         private void Button_Clicked(object sender, System.EventArgs e)
         {
-            produktsBasket.RemoveAt(DeleteBasketClicked); 
+            produktsBasket.RemoveAt(DeleteBasketClicked);
         }
 
         private void ListViewBasket_ItemSelected(object sender, SelectedItemChangedEventArgs e)
